9675cfd060dba3ece5700bbf1729f153834dd636
Daniel Aidan Cohen <168676041+Sashumi-6@users.noreply.github.com>
2025-04-21 13:02:47 +0200

    Changed database file File includes: link_database, which creates the connection and will return a connection singletion --maybe not.. see if better way? database, will have all the database manip accesseble thru instance() method

--- database.java
+++ database.java
@@ -1,15 +1,36 @@
+final class link_database {
+    //Eventually add all the data needed into the strings.
+    private final String dvdrental_DB_PROTO = "placeholder",
+                   dvdrental_DB_HOST = "placeholder",
+                   dvdrental_DB_PORT = "placeholder",
+                   dvdrental_DB_NAME = "placeholder",
+                   dvdrental_DB_USERNAME = "placeholder",
+                   dvdrental_DB_PASSWORD = "placeholder";
+
+    //private (connection data type) connection;
+    link_database() {
+        try {
+            //Try establish a connection
+        } catch (Exception e) { //Eventually make this an speciliazed value
+            //Connection failed => we should return a null value to the user.
+        }
+    }
+}
+
 public class database {
-    public static database use; // There is 100% a better name for this
+    private static database instance;
     //create a private connection object to the database
 
     database() {
-        if (use == null) {
-            use = new database();
-        }
-
         //Initilise the connection object then
         //Then we will use that object only
         //And then ofc use the singleton in the other
         //Classes to retreive data or whatever :3
+    }
+
+    //Use this method to access other methods
+    public database instance() {
+        if (instance == null) instance = new database();
+        return instance;
     }
 }
