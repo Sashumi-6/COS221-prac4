c7b43fae152c556c678fa86e8e1799216bfbb877
Pavan Naidoo <pavannaidoo6@gmail.com>
2025-04-21 13:32:27 +0200

    Basic Java GUI setup so far

--- Main.java
+++ Main.java
@@ -1,5 +1,39 @@
-public class Main {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class Main{
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Daniel and Pavan's GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(730,720);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
+        JTabbedPane tabPanel = new JTabbedPane(); // makes tabbed pane
+        
+
+        // Tab 1 - Home
+        
+       
+            JPanel home = new JPanel();
+                
+            
+
+            tabPanel.addTab("🏠Home", home);
+
+            
+
+
+        //////////////////////////////////////////////////////////
+
+    
+            
+            frame.add(tabPanel);
+           
+
+
         
     }
 }
--- notifications.java
+++ notifications.java
@@ -1,3 +1,14 @@
+import javax.swing.*;
 public class notifications {
+    public static JPanel notificationPanel(){
+    JPanel notiPanel = new JPanel();
+        
+    notiPanel.add(new JLabel(
+                    "New Tab Added"));
+
     
+    return notiPanel;
+   
+
+    }
 }
