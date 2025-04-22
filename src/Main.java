cd39ec3bc18ac2fb80a1bd5c76c8cc0821036e66
Daniel Aidan Cohen <168676041+Sashumi-6@users.noreply.github.com>
2025-04-22 09:50:45 +0200

    Main changes init database singleton -- establish db connection

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ import javax.swing.*;
 
 public class Main{
     public static void main(String[] args) {
+        //Initilise the database connection
+        database.instance();
+        // if connection fails?
+        // => System.exit(0), this happens in database file
+        //    Why? so we dont waste time rendering the JFrame
+
         JFrame frame = new JFrame();
         frame.setTitle("Daniel and Pavan's GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -13,41 +19,25 @@ public class Main{
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
 
-        JTabbedPane tabPanel = new JTabbedPane(); // makes tabbed pane
+        JTabbedPane tabPanel = new JTabbedPane();
         
-
         // Tab 1 - Home
-        
-       
            JPanel home = new JPanel();
            home.add(new JLabel("Welcome"));
-            
-
            tabPanel.addTab("🏠 Home", home);
-
-            
-
-
         //////////////////////////////////////////////////////////
 
         // Tab 2 - Employees
-        JPanel employee = employees.employeepanel();
-    
-       tabPanel.addTab("👤 Employees", employee);
-            
-            
-           
-            
-
-         //////////////////////////////////////////////////////////
+            JPanel employee = employees.employeepanel();
+            tabPanel.addTab("👤 Employees", employee);
+        //////////////////////////////////////////////////////////
          
-         // Tab 3 - Products
+        // Tab 3 - Products
          JPanel product = products.productpanel();
-    
          tabPanel.addTab("🛒 Products", product);
-
-         //////////////////////////////////////////////////////////
-         frame.add(tabPanel);
-         frame.setVisible(true);
+        //////////////////////////////////////////////////////////
+        
+        frame.add(tabPanel);
+        frame.setVisible(true);
     }
 }
